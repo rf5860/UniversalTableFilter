a82946ecf8dbcf0bf673b7cb9a320aa49e28e8a3
Robert Foy <robert.foy@gmail.com>
2017-12-12 04:59:25 +1100

    Add universal filter:

---
+++ src/universalTableFilter.js
@@ -0,0 +1,25 @@
+[...document.querySelectorAll('table')]
+    .forEach(table => new TableFilter(table, {
+        highlight_keywords: true,
+        no_results_message: true,
+
+        alternate_rows: true,
+        rows_counter: true,
+        loader: true,
+// TEMP
+        // grid layout customisation
+        grid_layout: {
+            width: '890px',
+            height: '400px',
+            filters: false,
+            cont_css_class: 'grd-main-cont',
+            tbl_head_css_class: 'grd-head-cont',
+            tbl_cont_css_class: 'grd-cont'
+        },
+// TEMP
+        extensions: [
+            { name: 'sort' },
+            { name: 'filtersVisibility', visible_at_start: false }
+        ],
+        base_path: 'chrome-extension://pnpjhhdjihjejnpmedbgngegphcklkag/'
+    }).init());
